ec701615a149f4538284abe69946293fc8902125
Jackson <635725626@qq.com>
2022-03-17 11:19:19 +0800

    add mrpc

--- train.sh
+++ train.sh
@@ -1,13 +1,14 @@
 name='baseline'
+dataset='mrpc'
 learning_rate='2e-5'
 epochs=4
 batch_size=64
 CUDA_VISIBLE_DEVICES=$1 python run.py --do_train \
 --model_type "bert" \
 --bert_model  "bert-base-uncased" \
---train_file "./data/quora/train" \
---dev_file "./data/quora/dev" \
---test_file "./data/quora/test" \
+--train_file "./data/$dataset/train" \
+--dev_file "./data/$dataset/dev" \
+--test_file "./data/$dataset/test" \
 --name $name   \
 --data 'quora'   \
 --do_lower_case \
@@ -16,6 +17,6 @@ CUDA_VISIBLE_DEVICES=$1 python run.py --do_train \
 --batch_size $batch_size \
 --max_length 65 \
 --fp16 --fptype O2 \
---save_dir "./save/$type/$epochs/$batch_size/$learning_rate/"
+--save_dir "./save/$dataset/$name/$epochs/$batch_size/$learning_rate/"
 
 # nohup sh train.sh > ./logs/1.txt 2>&1 &
